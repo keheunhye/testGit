9dd04e0f26618f907c2ade62823b7a6b41391fcd
bit <bit@bit-PC>
2016-04-05 11:03:27 +0900

    test01

---
+++ testGit01/src/main/java/Hello.java
@@ -0,0 +1,10 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello waterdog!");
+		System.out.println("으네가 왈왈");
+	}
+
+}
